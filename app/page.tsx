10456c43cab7a34b3519a4df8cbc35c881561a06
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2025-01-26 13:20:10 +0530

    updated login page for testing loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,31 @@
 "use client";
 
-import { useAuthModal, useSignerStatus, useUser } from "@account-kit/react";
-import AuthWrapper from "@/components/app/AuthWrapper";
+import { useSignerStatus } from "@account-kit/react";
 import Login from "@/components/app/Login";
-import { Button } from "@/components/ui/button";
-import { Loader } from "lucide-react";
-
+import { Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const user = useUser();
-  const { openAuthModal } = useAuthModal();
   const signerStatus = useSignerStatus();
-  const [isDashboard, setIsDashboard] = useState(false);
-  // const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const willStorage = localStorage.getItem("will-storage");
-    if (willStorage) {
-      setIsDashboard(true);
+    if (!signerStatus.isInitializing) {
+      setIsLoading(false);
     }
-  }, []);
+  }, [signerStatus.isInitializing]);
 
-  if (signerStatus.isInitializing && user) {
-    return <Loader className="animate-spin w-6 h-6" />;
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="animate-spin w-8 h-8" />
+      </div>
+    );
   }
 
   return (
-    <AuthWrapper>
-      <div className="flex flex-col items-center w-full h-full relative">
-        <Login />
-      </div>
-    </AuthWrapper>
+    <div className="flex flex-col items-center w-full h-full relative">
+      <Login />
+    </div>
   );
 }
--- components/app/Login.tsx
+++ components/app/Login.tsx
@@ -1,36 +1,45 @@
 "use client";
 import React, { useEffect } from "react";
-import { useAuthModal } from "@account-kit/react";
+import { useAuthModal, useSignerStatus, useUser } from "@account-kit/react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
 
 export default function Login() {
   const { openAuthModal } = useAuthModal();
+  const router = useRouter();
+  const user = useUser();
+  const signerStatus = useSignerStatus();
 
   useEffect(() => {
-    openAuthModal();
-  }, [openAuthModal]);
+    if (user) {
+      router.push("/buy-crypto"); // Redirect to the dashboard or another page if the user is logged in
+    } else if (!signerStatus.isInitializing) {
+      openAuthModal(); // Open auth modal by default if user is not logged in
+    }
+  }, [user, router, openAuthModal, signerStatus.isInitializing]);
+
+  if (signerStatus.isInitializing) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="h-8 w-8 animate-spin" />
+      </div>
+    );
+  }
 
   return (
     <Button
-      className="absolute"
+      className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       style={{
         color: "white",
         backgroundColor: "#000000",
         borderRadius: "42px",
-        top: "14%",
         fontSize: "18px",
         fontWeight: "400",
         padding: "24px 28px",
-        transform: "translateY(-50%)",
         zIndex: 10,
       }}
-      onClick={() => {
-        try {
-          openAuthModal();
-        } catch (error) {
-          console.log(error);
-        }
-      }}
+      onClick={openAuthModal}
     >
       Login
     </Button>
